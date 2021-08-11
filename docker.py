7d42c47da41d03601904d922f3e0b619da26a306
ShivaniMandloi <70172119+ShivaniMandloi@users.noreply.github.com>
2021-08-11 23:18:33 +0530

    Add files via upload

---
+++ docker.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+print("content-type: text/html")
+print()
+
+
+import cgi
+import subprocess as sp
+form = cgi.FieldStorage()
+cm=""
+output=""
+cm = form.getvalue("x")
+output = sp.getoutput(cm)
+#print(cm)
+print(output)
+
